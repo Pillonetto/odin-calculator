61be73b2c6a12cfde275e62d81789c8aa06fb3e3
Pillonetto <gabrielpillonetto@gmail.com>
2022-11-10 15:42:00 -0300

    Implements decimal numbers

--- scripts.js
+++ scripts.js
@@ -3,11 +3,13 @@ let displayResult = document.querySelector('.displayResult');
 let displayDigits = document.querySelector('.displayDigits');
 //To add event listeners to all buttons
 let numbers = document.querySelectorAll('.num');
+let decimal = document.querySelector('.dec');
 let operators = document.querySelectorAll('.op');
 let equals = document.querySelector('.equals');
 //To keep track of whether an operator has been selected, and what the selection was
 let hasOperator = false;
 let selectedOperation;
+let hasDecimal = false;
 //To store the first and second values inputed to calculator
 let a = 0;
 let b = 0;
@@ -17,9 +19,10 @@ let b = 0;
 numbers.forEach(number => 
     number.addEventListener('click', () => updateDisplay(number)));
 
+decimal.addEventListener('click', () => updateDisplayDecimal());
+
 operators.forEach(op =>     
     op.addEventListener('click', () => updateDisplayOp(op)));
-
 
 equals.addEventListener('click', () => realizeOperation(a, b));
 
@@ -32,8 +35,30 @@ function updateDisplay(number){
 
     if(displayDigits.textContent.length < 14)
         displayDigits.textContent += number.textContent;
-    else
+    else {
         alert("Maximum number of digits reached.");
+        clearDisplay();
+    }
+}
+
+function updateDisplayDecimal(){
+
+    displayDigits.textContent += '.';
+    disableDecimal();
+
+}
+
+function disableDecimal(){
+    decimal.disabled = true;;
+}
+
+function enableDecimal(){
+    decimal.disabled = false;
+}
+function treatDecimal(a){
+    if(a[0] == '.')
+        a = '0' + a;
+    return a;
 }
 
 function updateDisplayOp(op){
@@ -44,48 +69,89 @@ function updateDisplayOp(op){
 
     if(!hasOperator){
         
-        a = displayDigits.textContent;
-    
+        if(displayDigits.textContent == ''){
+            a = displayResult.textContent;
+            displayDigits.textContent += a;
+        }
+        else
+            a = treatDecimal(displayDigits.textContent);
+            
         displayDigits.textContent +=  ' ' + op.textContent + ' ';
+
+        //Update flags
         hasOperator = true;
         selectedOperation = op.textContent;
+        //Enables back decimals
+        enableDecimal();
     }
     else {
-        realizeOperation(a, b);
+
+        b = getB(a);
+        if(b.length == 0)
+            return alert("Invalid Operation!");
+
+        if(displayDigits.textContent != '')
+            realizeOperation(a, b);
         //Updates to new display content
-        a = displayResult.textContent;
+        a = treatDecimal(displayResult.textContent);
+        console.log(a);
 
         displayDigits.textContent = a + ' ' + op.textContent + ' ';
         selectedOperation = op.textContent;
+        hasOperator = true;
+        enableDecimal();
 
     }
 
 }
 
-function realizeOperation(a, b){
-    
-    //Length of number a + ' ' + operator + ' '
-    b = displayDigits.textContent.substring(a.length + 3);
-    a = parseInt(a);
-    b = parseInt(b);
+function getB(a){
+    return displayDigits.textContent.substring(a.length + 3);
+}
 
+function realizeOperation(a, b){
+    //Length of number a + ' ' + operator + ' '
+    b = getB(a);
+    
+    if(b.length == 0 || a.length == 0)
+        return alert('Invalid Operation!');
+    
+    a = parseFloat(a);
+    b = parseFloat(b);
+
+    //If user is trying to divide by 0
     if(selectedOperation == '/' && b == 0){
-        clearDisplay();
         return alert('Are you trying to crash my calculator? Wow.');
     }
+    //If it has operator, operate. If it doesnt, just copy the number itself
     toBeDisplayed = (hasOperator) ? operate(a, b, selectedOperation) : displayDigits.textContent;
 
+    //Making sure the result isnt bigger than display
+    if(toBeDisplayed.toString().length > 8)
+        return alert('Result is too long');
+    
     displayResult.textContent = toBeDisplayed;
 
+    hasOperator = false;
+
+    clearDisplayUpper();
+
+}
+
+function clearDisplayUpper(){
+    displayDigits.textContent = '';
+    selectedOperation = '';
+    enableDecimal();
 }
 
 function clearDisplay(){
     a = 0;
     b = 0;
-    displayResult.textContent = ' ';
-    displayDigits.textContent = ' ';
+    // displayResult.textContent = '';
+    displayDigits.textContent = '';
     hasOperator = false;
     selectedOperation = '';
+    enableDecimal();
 }
 
 // Operation functions
